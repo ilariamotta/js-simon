1b5a8842ab7ba843d9ea15cdb6b4df8867ad3f3b
ilariamotta <ilaria.motta93@gmail.com>
2025-11-07 17:04:02 +0100

    fix: generazione numeri

--- js/script.js
+++ js/script.js
@@ -1,17 +1,48 @@
 const countdown = document.querySelector("#countdown")
+const numMem = document.querySelector("#numMem");
 
+const form = document.querySelector("form");
+const btn = document.querySelector("button");
+
+//NUMERI INPUT
+const numInputOne = document.querySelector("#n1");
+const numInputTwo = document.querySelector("#n2");
+const numInputThree = document.querySelector("#n3");
+const numInputFour = document.querySelector("#n4");
+const numInputFive = document.querySelector("#n5");
+
+
+// NUMERI DA RICORDARE
 const memo1 = document.querySelector("#memo1")
 const memo2 = document.querySelector("#memo2")
 const memo3 = document.querySelector("#memo3")
 const memo4 = document.querySelector("#memo4")
 const memo5 = document.querySelector("#memo5")
 
-memo1.innerHTML = Math.floor(Math.random() * 99) + 1;
-memo2.innerHTML = Math.floor(Math.random() * 99) + 1;
-memo3.innerHTML = Math.floor(Math.random() * 99) + 1;
-memo4.innerHTML = Math.floor(Math.random() * 99) + 1;
-memo5.innerHTML = Math.floor(Math.random() * 99) + 1;
+// GENERAZIONE NUMERI
 
+const numGen = [];
+
+for (let i = 0; i < 5; i++) {
+    const num = Math.floor(Math.random() * 99) + 1;
+    if (!numGen.includes(num)) {
+        numGen.push(num);
+    } if (numGen.length === 5){
+        break;
+    }
+
+}
+
+console.log(numGen)
+//STAMPA NUMERI IN PAGINA
+memo1.innerHTML = numGen[0];
+memo2.innerHTML = numGen[1];
+memo3.innerHTML = numGen[2];
+memo4.innerHTML = numGen[3];
+memo5.innerHTML = numGen[4];
+
+
+//FUNZIONE COUNTDOWN
 let count = 30;
 countdown.innerHTML = 30;
 const intervalId = setInterval(function () {
@@ -20,12 +51,29 @@ const intervalId = setInterval(function () {
         countdown.innerHTML = count;
 
     } else if (count === 0) {
-        countdown.innerHTML = "STOP AL TEMPO";
-        clearInterval(intervalId)
+        clearInterval(intervalId);
+        countdown.classList.add("d-none");
+        numMem.classList.add("d-none");
 
     }
-
 }, 1000);
+
+
+
+
+// form.addEventListener("submit", function (event) {
+// event.preventDefault();
+
+// for (i = 0; i < arrayMemo.length; i++) {
+    
+//     }
+// }
+
+
+// });
+
+
+
 
 
 
