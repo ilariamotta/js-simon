e149faafd78b38416960a84656f402eb4dde8afe
ilariamotta <ilaria.motta93@gmail.com>
2025-11-07 15:51:46 +0100

    feat: generazione numeri random

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
+const memo1 = document.querySelector("#memo1")
+const memo2 = document.querySelector("#memo2")
+const memo3 = document.querySelector("#memo3")
+const memo4 = document.querySelector("#memo4")
+const memo5 = document.querySelector("#memo5")
 
+memo1.innerHTML = Math.floor(Math.random() * 99) + 1;
+memo2.innerHTML = Math.floor(Math.random() * 99) + 1;
+memo3.innerHTML = Math.floor(Math.random() * 99) + 1;
+memo4.innerHTML = Math.floor(Math.random() * 99) + 1;
+memo5.innerHTML = Math.floor(Math.random() * 99) + 1;
 
+console.log(memo1, memo2, memo3, memo4, memo5);
 
 
 
